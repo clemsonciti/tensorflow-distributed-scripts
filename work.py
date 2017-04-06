c20d579bc98d1dd517ffd5c5fafd0caddee3a0d2
Ashwin Srinath <atrikut@clemson.edu>
2017-04-06 12:36:33 -0400

    remove loop in work.py

--- work.py
+++ work.py
@@ -5,13 +5,11 @@ x = tf.placeholder(tf.float64, 10000000)
 
 with tf.device("/job:worker/task:0/gpu:1"):
     first_batch = tf.slice(x, [0], [5000000])
-    for i in range(10000):
-        mean1 = tf.reduce_mean(first_batch)
+    mean1 = tf.reduce_mean(first_batch)
 
 with tf.device("/job:worker/task:1/gpu:1"):
     second_batch = tf.slice(x, [5000000], [-1])
-    for i in range(10000):
-        mean2 = tf.reduce_mean(second_batch)
+    mean2 = tf.reduce_mean(second_batch)
     mean = (mean1 + mean2) / 2
 
 with tf.Session("grpc://localhost:2222", config=tf.ConfigProto(log_device_placement=True)) as sess:
